482a73bebdbaf690b0a55680a72fec3c5ee0b4d9
Rob L <viewpl@gmail.com>
2015-04-02 16:41:31 +1100

    Fix docker run command

--- runrdm.sh
+++ runrdm.sh
@@ -12,13 +12,13 @@ mkdir -p /opt/redmine/data
 docker run -p 443:443 -d --name=redmine \
 	-e REDMINE_HTTPS=true \
 	-e REDMINE_HTTPS_HSTS_MAXAGE=31536000 \
+	-e DB_TYPE=mysql \
+	-e DB_HOST=localhost \
 	-e DB_NAME=redmine_production \
 	-e DB_USER=redmine \
 	-e DB_PASS=5adT$x6Z3TP \
-	#-e SMTP_USER=[your email] \
-	#-e SMTP_PASS=[your password] \
 	-e SSL_CERTIFICATE_PATH=/home/redmine/data/certs/redmine.crt \
 	-e SSL_KEY_PATH=/home/redmine/data/certs/redmine.key \
 	-e SSL_DHPARAM_PATH=/home/redmine/data/certs/dhparam.pem \
 	-v /opt/mysql/data:/var/lib/mysql \
-	-v /opt/redmine/data:/home/redmine/data viewpl/redmine
+	-v /opt/redmine/data:/home/redmine/data viewpl/docker-redmine
